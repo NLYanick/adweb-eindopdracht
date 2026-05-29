00a41678610d64402c02f04103c2fd8079d481fb
MatthijsCr <msj.cromwijk@student.avans.nl>
2026-05-29 13:49:12 +0200

    totally more readable

---
+++ app/budgetbook/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+import Image from "next/image";
+import Link from 'next/link'
+import { getUsers } from "../services/user-service";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+    const [users, setUsers] = useState<any[]>([]);
+
+    useEffect(() => {
+    getUsers()
+    .then(users => {
+        setUsers(users);
+    });
+    }, [])
+
+    return (
+        <main className="p-24">
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+            <ul>
+                {users.map((user: any) => (
+                <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </main>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import Link from 'next/link'
 import { getUsers } from "./services/user-service";
 import { useEffect, useState } from "react";
 
@@ -17,12 +18,7 @@ export default function Home() {
   return (
     <main className="p-24">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
-      <ul>
-        {users.map((user: any) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <Link href="/budget">Dashboard</Link>
     </main>
   );
 }
