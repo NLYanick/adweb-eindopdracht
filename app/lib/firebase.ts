29069d4b7d3542a3acb4a6f4ecfcc4b1accd066a
MatthijsCr <msj.cromwijk@student.avans.nl>
2026-05-29 13:36:09 +0200

    push

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -13,10 +13,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 export {
-    analytics,
     db
 };
