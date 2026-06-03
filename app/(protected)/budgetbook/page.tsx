e887f0936568b8229aae492f18d54a20c80516b1
MatthijsCr <msj.cromwijk@student.avans.nl>
2026-06-03 11:03:46 +0200

    trans

--- app/(protected)/budgetbook/page.tsx
+++ app/(protected)/budgetbook/page.tsx
@@ -9,43 +9,45 @@ import { useSearchParams } from "next/navigation";
 
 export default function Home() {
   const searchParams = useSearchParams();
-  const showArchived = searchParams.get("archived") === "true";
+  const [showArchived, setArchived] = useState(false);
   const { loading, user } = useAuth();
   const [budgetbooks, setBudgetBooks] = useState<Budgetbook[]>([]);
 
   useEffect(() => {
     if (loading || !user) return;
-    const unsubscribe = watchBudgetBooks(user.uid, showArchived ,(budgetbooks) => {
+    const unsubscribe = watchBudgetBooks(user.uid, showArchived, (budgetbooks) => {
       setBudgetBooks(budgetbooks);
     });
 
     return () => unsubscribe();
-  }, [user, loading,showArchived]);
+  }, [user, loading, showArchived]);
 
   return (
     <main className="p-24">
       <div className="flex items-center gap-4 justify-between">
         <div>
-        <h1 className="text-3xl font-bold underline">
-          {showArchived ? "Archived books" : "Budget books"}
-        </h1>
-        <Link
-          href={showArchived ? "/budgetbook" : "/budgetbook?archived=true"}
-          className="text-sm text-gray-500 hover:text-black transition-colors"
-        >
-          {showArchived ? "Show active" : "Show archived"}
-        </Link>
+          <h1 className="text-3xl font-bold underline">
+            {showArchived ? "Archived books" : "Budget books"}
+          </h1>
+          <button
+            onClick={() => setArchived(!showArchived)}
+            className="text-sm text-gray-500 hover:text-black transition-colors"
+          >
+            {showArchived ? "Show active" : "Show archived"}
+          </button>
         </div>
-        <Link 
-          href="/budgetbook/create"
-          className="bg-blue-500 text-white p-2 rounded  hover:bg-blue-400"
-        >
-          Create new
-        </Link>
+        {!showArchived &&
+          <Link
+            href="/budgetbook/create"
+            className="bg-blue-500 text-white p-2 rounded  hover:bg-blue-400"
+          >
+            Create new
+          </Link>
+        }
       </div>
 
       <ul className="mt-4 flex flex-col gap-2">
-          {budgetbooks.map((budgetbook) => (
+        {budgetbooks.map((budgetbook) => (
           <BudgetBookItem
             key={budgetbook.uid}
             budgetbook={budgetbook}
